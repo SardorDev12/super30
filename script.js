578fe3d467afbe8e4582ecdc2eb1e5bd05a3afbf
SardorDev12 <wiut00012453@gmail.com>
2022-05-25 21:43:51 +0500

    about us page

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const burgerMenu = document.querySelector(".burger-icon");
 const quit = document.querySelector(".quit");
 
-burgerMenu.addEventListener("click", () => {
+burgerMenu.addEventListener("click", (e) => {
+  e.preventDefault();
   document.querySelector(".disabled").classList.add("active");
 });
-quit.addEventListener("click", () => {
+quit.addEventListener("click", (e) => {
+  e.preventDefault();
   document.querySelector(".disabled").classList.remove("active");
 });
